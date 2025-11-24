9a1da05a21c5da3d6d7ec94423bae597e003da06
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-24 23:20:33 +0600

    About page created

---
+++ src/components/Extra/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, asperiores.</p>
+        </div>
+    );
+};
+
+export default About;
