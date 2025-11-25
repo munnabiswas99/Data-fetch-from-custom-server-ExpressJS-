8e177863d9d8db557956ce0b355be2a65a7ce080
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-25 11:05:05 +0600

    Career Page created

---
+++ src/components/Extra/Career.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Career = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    );
+};
+
+export default Career;
